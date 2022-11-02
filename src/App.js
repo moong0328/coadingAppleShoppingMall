5bbf021c3cfcb600ac222e390dd3d85e7e001139
Woong Hwan Kim <samkj1@naver.com>
2022-11-02 21:31:18 +0900

    Shop day 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import shoeData from './data';
+import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
+import Detail from './routes/Detail'
 
 function App() {
+
+  let [shoes] = useState(shoeData)
+  let navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">A Shoe</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => {
+              navigate('/')
+            }}>Home</Nav.Link>
+            <Nav.Link onClick={() => {
+              navigate('/detail')
+            }}>Detail</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      {/* <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link> */}
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg"></div>
+            <Container>
+              <Row>
+                {
+                  shoes.map((item, index) => {
+                    let imageUrl = "https://codingapple1.github.io/shop/shoes" + (index + 1) + ".jpg"
+                    return (
+                      <Shoes imageUrl={imageUrl} item={item}></Shoes>
+                    )
+                  })
+                }
+              </Row>
+            </Container>
+          </>
+        } />
+        <Route path="/detail" element={<Detail></Detail>} />
+        <Route path="/*" element={<div>Error Page</div>}></Route>
+
+        {/* Nested Routes
+          * 여러 유사한 페이지가 필요 할 때
+        */}
+        <Route path="/about" element={<About></About>}>
+          <Route path="member" element={<div>Member Info</div>}/>
+          <Route path="location" element={<div>Location Info</div>}/>
+        </Route>
+
+        <Route path="/event" element={<Event></Event>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}/>
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
 
+const Shoes = (props) => {
+  return ( 
+    <Col md="4">
+      <img src={props.imageUrl} width="80%" alt="shoeImage"/>
+      <h4>{props.item.title}</h4>
+      <p>{props.item.content}</p>
+      <p>{props.item.price}</p>
+    </Col>
+  )
+}
+
+const About = (props) => {
+  return (
+    <div>
+      <h4>About...</h4>
+      {/* <Outlet></Outlet>
+        * Nested Routes 구조로 이루어진 Route Component 자리      
+      */}
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+const Event = (props) => {
+  return (
+    <div>
+      <h4>Today Event</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
 export default App;
